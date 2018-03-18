ca0069939124a947548bb0a598151b99e623e0ce
smartwebdev2017 <bern.oliver.630@yandex.com>
2018-03-18 19:42:06 +0100

    update-20

--- src/app/theme/components/baSidebar/BaSidebarCtrl.js
+++ src/app/theme/components/baSidebar/BaSidebarCtrl.js
@@ -621,7 +621,7 @@
                     $('.al-main').css('padding-left', '0px');
                 }
                 $timeout(function(){
-                    $(window).trigger('resize');
+                    //$(window).trigger('resize');
                 });
             }
         }
